3348eea4aa51d242459ee06b00c6224f0e1a1c27
Ebony Johnson <ejohnson23451@gmail.com>
2022-01-07 17:40:53 -0500

    Created Srping Boot Project

---
+++ KawaiiAnimes/src/main/java/com/skilldistillery/anime/KawaiiAnimesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.anime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KawaiiAnimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KawaiiAnimesApplication.class, args);
+	}
+
+}
---
+++ KawaiiAnimes/src/test/java/com/skilldistillery/anime/KawaiiAnimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.anime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KawaiiAnimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
