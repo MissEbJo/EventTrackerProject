47a4d48cfd3694f3df300c7304db5f48d3606eda
Ebony Johnson <ejohnson23451@gmail.com>
2022-01-07 17:34:31 -0500

    Stubbed Anime entity, tested mappings, and created JPA functionality

---
+++ AnimesJPA/src/test/java/com/skilldistillery/anime/entities/AnimeTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.anime.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AnimeTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Anime anime;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("AnimesJPA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		anime = em.find(Anime.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		anime = null;
+	}
+
+	@Test
+	@DisplayName("test Anime mappings")
+	void test1() {
+		assertNotNull(anime);
+		assertEquals("Maidsama", anime.getTitle());
+		
+	}
+
+}
